1874d7035a7a4e73559724cabfe6b9b2ff4eb271
Evgeniy Altunin <20day02@rambler.ru>
2020-06-15 13:50:46 +0300

    clear folder

--- UI5Task/MasterDetailApp/webapp/controller/DetailObjectNotFound.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define([
-	"./BaseController"
-], function (BaseController) {
-	"use strict";
-
-	return BaseController.extend("Main.MasterDetailApp.controller.DetailObjectNotFound", {});
-});
